f0e33a56035636ccaf98e9336bb09f0c465e126e
Tycalwayslove <1186446929@qq.com>
2019-06-27 00:05:44 +0800

    <fix> 整理

--- vue_ssr_sport/nuxt.config.js
+++ vue_ssr_sport/nuxt.config.js
@@ -1,112 +1,112 @@
-const path = require('path');
+const path = require('path')
 module.exports = {
-    mode: 'spa',
-    server: {
-        port: 8080, // default: 3000
-        host: 'localhost', // default: localhost
-    },
-    /*
-     ** Headers of the page
-     */
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1,user-scalable=no' },
-            {
-                hid: 'description',
-                name: 'description',
-                content: process.env.npm_package_description || '',
-            },
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    },
-    /*
-     ** Customize the progress-bar color
-     */
-    loading: { color: '#fff' },
-    /*
-     ** Global CSS
-     */
-    css: ['@/assets/css/index.scss'],
-    /*
-     ** Plugins to load before mounting the App
-     */
-    plugins: [{
-            src: '~/plugins/axios',
-            ssr: false,
-        },
-        // {
-        //     src: '~/plugins/moment.js',
-        //     ssr: false,
-        // },
+  mode: 'spa',
+  server: {
+    host: 'localhost',
+    port: 8080
+  },
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1,user-scalable=no'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios',
-        '@nuxtjs/eslint-module',
-        '@nuxtjs/style-resources', // with styleResources
-    ],
-    /*
-     ** Nuxt Style Resources (sass,less,stylus )
-     ** github: https://github.com/nuxt-community/style-resources-module
-     */
-    styleResources: {
-        // your settings here
-        scss: [
-            '@/assets/css/variable.scss',
-            '@/assets/css/utils.scss',
-            // use underscore "_" & also file extension ".scss"
-        ],
-    },
-    /*
-     ** Axios module configuration
-     ** See https://axios.nuxtjs.org/options
-     */
-    axios: {
-        proxy: true,
-        prefix: '/api',
-        credentials: true,
-    },
-    // proxy: {
-    //     '/api/': {
-    //         target: 'http://3.1.85.182:8080',
-    //         changeOrigin: true,
-    //         pathRewrite: {
-    //             '^/api': '',
-    //         },
-    //     },
-    // },
-    /*
-     ** Build configuration
-     */
-    build: {
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {
-            /*
-             ** alias
-             */
-            config.resolve.alias.components = path.resolve(__dirname, 'components');
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: ['@/assets/css/index.scss'],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    {
+      src: '~/plugins/axios',
+      ssr: false
+    }
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  // axios: {
+  //   baseURL: process.env.NODE_ENV == 'production'? "http://3.1.85.182:8080":"http://localhost:8080"
+  // },
+  proxy: {
+    '/api': {
+      target: 'http://3.1.85.182:8080/api',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    }
+  },
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources' // with styleResources
+  ],
+  /*
+   ** Nuxt Style Resources (sass,less,stylus )
+   ** github: https://github.com/nuxt-community/style-resources-module
+   */
+  styleResources: {
+    // your settings here
+    scss: [
+      '@/assets/css/variable.scss',
+      '@/assets/css/utils.scss'
+      // use underscore "_" & also file extension ".scss"
+    ]
+  },
 
-            /*
-             **  Run ESLint on save
-             */
-            if (ctx.isDev && ctx.isClient) {
-                config.module.rules.push({
-                    enforce: 'pre',
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    exclude: /(node_modules)/,
-                    options: {
-                        fix: true,
-                    },
-                });
-            }
-        },
-    },
-};
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      /*
+       ** alias
+       */
+      config.resolve.alias.components = path.resolve(__dirname, 'components')
+
+      /*
+       **  Run ESLint on save
+       */
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
+  }
+}
--- vue_ssr_sport/plugins/axios.js
+++ vue_ssr_sport/plugins/axios.js
@@ -1,19 +1,21 @@
-export default function(app) {
+// import * as axios from 'axios'
+export default function({ app }) {
+  console.log(app)
   const axios = app.$axios
   // 基本配置
-  // axios.defaults.baseUrl = "http://3.1.85.182:8080"
+  // axios.defaults.baseURL = 'http://3.1.85.182:8080'
   axios.defaults.timeout = 10000
   axios.defaults.headers.post['Content-Type'] =
     'application/x-www-form-urlencoded'
 
   // 请求回调
   axios.onRequest(config => {
-    // console.log(config)
+    console.log(config)
   })
 
   // 返回回调
   axios.onResponse(res => {
-    // console.log(res)
+    console.log(res)
     if (res.status === 200) {
       return res.data.Data
     }
